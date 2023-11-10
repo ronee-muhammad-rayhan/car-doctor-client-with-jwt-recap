b72f934dd4b205b401e6b0ed9ecae4b11e0f181a
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-10 14:08:47 +0600

    63_5-1 Sort services by price and double data type

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,8 +4,9 @@ import useAuth from "./useAuth";
 import { useNavigate } from "react-router-dom";
 
 
-const axiosSecure = axios.create({
-    baseURL: 'https://car-doctor-server-topaz-one.vercel.app',
+export const axiosSecure = axios.create({
+    baseURL: 'http://localhost:5004',
+    // baseURL: 'https://car-doctor-server-topaz-one.vercel.app',
     withCredentials: true
 });
 
@@ -20,7 +21,7 @@ const useAxiosSecure = () => {
             if (error.response.status === 401 || error.response.status === 403) {
                 console.log('logout the user')
                 logOut()
-                    .then(() => { 
+                    .then(() => {
                         navigate('/login')
                     })
                     .catch(error => console.log(error))
